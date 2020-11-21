3cf6f86848d9c570e266bf41d57ecee619667294
AymanTareq.97 <tareq.ctec02@gmail.com>
2020-11-21 20:37:18 +0600

    created a new model for customer

--- essEcommerce/models.py
+++ essEcommerce/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
+class Customer(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=250)
+    address = models.CharField(512)
+    mobile = models.CharField(12)
+
+    def __str__(self):
+        return self.name
+    
 class Product(models.Model):
     name = models.CharField(max_length=250, null=True,blank=True)
     price = models.IntegerField()
@@ -8,6 +18,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    @property
+    def img(self):
+        try:
+            img_url = self.image.url
+        except:
+            img_url = ''
+        return img_url
 
     def get_absolute_url(self):
         return reverse("Product_detail", kwargs={"pk": self.pk})
